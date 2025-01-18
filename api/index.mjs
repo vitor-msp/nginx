4220a152d59968d07d4e293d8b81ff8c6ddd8cd5
Vitor Mateus <vitor7jan@gmail.com>
2025-01-18 17:44:48 -0300

    added reverse proxy with load balancer

---
+++ api/index.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+import ip from "ip";
+
+const port = 8080;
+const color = process.argv[2];
+const api = express();
+const response = {
+  ip: ip.address(),
+  color,
+};
+
+api.get("/api", (req, res) => {
+  console.log(req.headers);
+  res.json(response);
+});
+
+api.put("/api", (req, res) => {
+  console.log(req.body);
+  res.send(response);
+});
+
+api.listen(port, () => console.log(`api started on ${port}`));
